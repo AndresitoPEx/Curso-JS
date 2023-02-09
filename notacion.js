d9a2170c74b46d30602585cfec8d6f2b65b7cff2
Jorge Romo <andresrt952@gmail.com>
2023-02-08 22:34:28 -0500

    que es la notacion

---
+++ notacion.js
@@ -0,0 +1,30 @@
+//Notación en JS
+//Son un conjunto de reglas y convenciones que se utilizan para escribir codigo de manera clara, legible y estructurada.
+
+//; --> Delimita el final de una linea
+let mensaje = "Hola amigos";
+console.log(mensaje);
+
+//. --> Se usa en objetos para acceder a sus atributos
+const mascota = {
+    nombre : "Firulais"
+}
+console.log(mascota.nombre);
+
+//[] --> Se usa en Arrays
+let numeros = [1,2,3,4,5];
+console.log(numeros[3]);
+
+//() --> Se usa en funciones
+function suma(num1, num2) {
+    //contenido de la funcion
+}
+suma(4,5);
+
+//{} --> Se usa en objetos, funciones y estructuras de control.
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
