b7fbb7e66fdc68463d1b1b0321c27d7a7002ca21
Jorge Romo <andresrt952@gmail.com>
2023-02-10 09:13:17 -0500

    que es un array

---
+++ Arrays.js
@@ -0,0 +1,17 @@
+//Qué es un Array?
+//Es un conjunto de elementos.
+//Esta asignado a una variable.
+
+//let array = [1,"dos", true, [3,4]];
+
+let paises = ["Peru", "Mexico","Argentina","Brasil"];
+
+paises.push("Chile");
+paises.unshift("Ecuador");
+console.log(paises);
+
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,11 @@
+//Declaracion de variables y escritura dinamica
+const persona = {
+    nombre: "Anita"
+};
+//persona = {nombre: "Pedro"};
+persona.nombre = "Pedro";
+console.log(persona);
+
+
+
+
